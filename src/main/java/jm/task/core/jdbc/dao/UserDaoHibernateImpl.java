7e167984fcb4745ca5a674a2474e7417b1bedf58
Waterfall1991 <ykyreniy_ded@mail.ru>
2023-05-19 21:35:15 +0300

    P.P.1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,120 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+
     public UserDaoHibernateImpl() {
+
 
     }
 
-
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.getSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(
+                    "CREATE TABLE IF NOT EXISTS users" +
+                    "(id SERIAL PRIMARY KEY, " +
+                    "name VARCHAR(50)," +
+                    " lastname VARCHAR(50)," +
+                    "age SMALLINT)").executeUpdate();
 
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(
+                    "DROP TABLE IF EXISTS users"
+            ).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSession()) {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (Session session = Util.getSession()) {
+            transaction = session.beginTransaction();
+            session.remove(session.get(User.class, id));
+            session.getTransaction().commit();
 
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> allUsers = null;
+        Transaction transaction = null;
+        try (Session session = Util.getSession()) {
+            transaction = session.beginTransaction();
+            allUsers = session.createQuery(
+                    "FROM  User").getResultList();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return allUsers;
+
     }
+
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.getSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery(
+                            "DELETE FROM User")
+                    .executeUpdate();
+            session.getTransaction().commit();
 
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,46 @@
 package jm.task.core.jdbc.util;
 
-public class  Util {
-    public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-   public static final String USERNAME = "postgres";
-   public static final String PASSWORD = "01020129Sv";
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.util.Properties;
+
+public class Util {
+
+    public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "01020129Sv";
+    private static SessionFactory sessionFactory;
+    private static Properties properties = new Properties();
+
+
+    static {
+        configureProperties();
+    }
+
+    static {
+        sessionFactory = new Configuration()
+                .setProperties(properties)
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    private static void configureProperties() {
+        try {
+            properties.load(Util.class.getClassLoader().getResourceAsStream("111.properties"));
+        } catch (IOException ignore) {
+            throw new RuntimeException(ignore);
+        }
+
+    }
 }
-    // реализуйте настройку соеденения с БД
+
+
+// реализуйте настройку соеденения с БД
